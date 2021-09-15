bcaea6882ad8f0ff80ad23fb75bd41c5969740c4
unknown <knitish123@gmail.com>
2021-09-15 20:59:39 +0530

    Backend

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { response, request } = require("express");
+const stripe = require("stripe")(
+  "sk_test_51JZwhvSFORGBY1T3KCb2tKQGOfkm5BcjI15UJCsYwTtsnx7zoZpZt4nC4EmAHuR6ncwSVsZO7jOIj9JET1UfkAcF000Ku7gnsm"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// API Config
+
+const app = express();
+
+// Middleware
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+// API Routes
+
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("Payment request recived Boom!! for this amount", total);
+});
+// Listen command
+
+exports.api = functions.https.onRequest(app);
+
+// example end point
+
+// http://localhost:5001/challenge-44d4f/us-central1/api
